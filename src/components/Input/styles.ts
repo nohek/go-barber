613c47c6aceeaad1389e1cb710060ae0d839c257
Francyelle <francyellem64@gmai.com>
2021-07-26 10:13:38 -0300

    useCallback
    usabilidade

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,33 +1,46 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
-  background: #232129;
-  border-radius: 10px;
-  border: 2px solid #232129;
-  padding: 16px;
-  width: 100%;
-  color: #666360;
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
 
+export const Container = styled.div<ContainerProps>`
   display: flex;
   align-items: center;
+  background: #232129;
+  border-radius: 10px;
+  padding: 16px;
+  width: 100%;
+
+  color: #666360;
+  border: 2px solid #232129;
 
   & + div {
     margin-top: 8px;
   }
+
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
 
   input {
     color: #f4ede8;
     flex: 1;
     background: transparent;
     border: 0;
-
-    &::placeholder {
-      color: #666360;
-    }
   }
 
   svg {
     margin-right: 16px;
-    color: #666360;
   }
 `;
